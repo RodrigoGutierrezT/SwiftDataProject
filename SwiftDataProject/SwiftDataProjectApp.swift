93c56610e3c7e00224d945ffd1a036229177f3f7
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-07-31 19:14:16 -0400

    Initial commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Rodrigo on 31-07-24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
