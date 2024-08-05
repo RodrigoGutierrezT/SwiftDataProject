64d6299319780d8d48955e5ac56eb66d01b33942
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-08-04 23:01:40 -0400

    feat: Add User model

--- SwiftDataProject/SwiftDataProjectApp.swift
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -5,6 +5,7 @@
 //  Created by Rodrigo on 31-07-24.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct SwiftDataProjectApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: User.self)
     }
 }
